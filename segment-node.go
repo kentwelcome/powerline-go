1e28bab9e16c68d485d42e5763aefb63d0bcaee5
Kent Huang <kent@infuseai.io>
2018-11-06 01:56:02 +0800

    [Feature] Rewrite segment node logic  - Parse node version by using `node --version`

--- segment-node.go
+++ segment-node.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"os"
+	"os/exec"
+	"strings"
 )
 
 const pkgfile = "./package.json"
@@ -12,16 +11,21 @@ type packageJSON struct {
 	Version string `json:"version"`
 }
 
+func parseNodeVersion() (string, error) {
+	nodePath, err := exec.LookPath("node")
+	out, err := exec.Command(nodePath, "--version").Output()
+	if err != nil {
+		return "", err
+	}
+	return strings.Replace(string(out), "\n", "", -1), nil
+}
+
 func segmentNode(p *powerline) {
-	stat, err := os.Stat(pkgfile)
-	if err == nil && !stat.IsDir() {
-		pkg := packageJSON{"!"}
-		raw, err := ioutil.ReadFile(pkgfile)
-		if err == nil {
-			json.Unmarshal(raw, &pkg)
-		}
+	// stat, err := os.Stat(pkgfile)
+	nodeVersion, err := parseNodeVersion()
+	if err == nil && nodeVersion != "" {
 		p.appendSegment("node-version", segment{
-			content:    pkg.Version + " \u2B22",
+			content:    nodeVersion + " \u2B22",
 			foreground: p.theme.NodeFg,
 			background: p.theme.NodeBg,
 		})
